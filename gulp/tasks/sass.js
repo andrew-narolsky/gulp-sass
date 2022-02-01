7650058bb80232f437f084a3656c5e4254771f33
Andrew <andrew.narolsky@gmail.com>
2022-02-01 13:37:21 +0200

    update project

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,16 +1,18 @@
+const sass = require('gulp-sass')(require('sass'));
+
 module.exports = function () {
-  $.gulp.task('sass', function () {
-    return $.gulp.src('sass/style.scss')
-        .pipe($.gp.plumber())
-        .pipe($.gp.sourcemaps.init())
-        .pipe($.gp.sass())
-        .pipe($.gp.autoprefixer({
-            overrideBrowserslist: ['last 2 versions']
-        }))
-        .pipe($.gp.csso())
-        .pipe($.gp.rename('style.min.css'))
-        .pipe($.gp.sourcemaps.write(''))
-        .pipe($.gulp.dest('build/css'))
-        .pipe($.browserSync.stream());
-  });
+    $.gulp.task('sass', function () {
+        return $.gulp.src('sass/style.scss')
+            .pipe($.gp.plumber())
+            .pipe($.gp.sourcemaps.init())
+            .pipe(sass())
+            .pipe($.gp.autoprefixer({
+                overrideBrowserslist: ['last 2 versions']
+            }))
+            .pipe($.gp.csso())
+            .pipe($.gp.rename('style.min.css'))
+            .pipe($.gp.sourcemaps.write(''))
+            .pipe($.gulp.dest('build/css'))
+            .pipe($.browserSync.stream());
+    });
 };
